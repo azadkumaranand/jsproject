ed5b5d842759dc99ff4f1e16b4b212b33fd6ff50
azadanand9798 <azadanand9798@gmail.com>
2024-04-13 12:47:44 +0530

    crud operations

--- crdu-operation/script.js
+++ crdu-operation/script.js
@@ -1,16 +1,34 @@
 const input = document.querySelector('#inpu-field');
 
-const table = document.querySelector('.table');
+const table = document.querySelector('.table-body');
 const button = document.querySelector('.button');
 
+let editeitem = null;
 
+function editcontent(id) {
+    editeitem = document.getElementById(`${id}`);
+    input.value = editeitem.innerText;
+    // console.log(typeof(id))
+    // let editeitem = document.querySelector(`#${id}`);
+}
+
+let val = input.value;
+console.log("inputvalue: ", val);
+let count = 1;
 button.addEventListener('click', ()=>{
     let val = input.value;
-    console.log(val);
-    table.innerHTML = table.innerHTML + `<tr>
-    <td style="width: 85%;">${val}</td>
-    <td style="width: 7.5%; color: green;"><i class="fa-solid fa-pen-to-square"></i></td>
-    <td style="width: 7.5%; color: red;"><i class="fa-solid fa-trash"></i></td>
-    </tr>`;
+    // console.log(val);
+    if (editeitem) {
+        editeitem.innerHTML = val;
+        editeitem = null;
+    }else{
+        table.innerHTML += `<tr>
+        <td style="width: 85%;" id="${count}">${val}</td>
+        <td style="width: 7.5%; color: green;"><i class="fa-solid fa-pen-to-square" onclick="editcontent(${count})"></i></td>
+        <td style="width: 7.5%; color: red;"><i class="fa-solid fa-trash"></i></td>
+        </tr>`;
+        count++;
+    }
+    
     input.value = '';
 })
